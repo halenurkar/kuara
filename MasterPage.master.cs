8b756dc99b2d2d5911ba7137dfeb16b8509cf75e
halenur,kar <358782@ogr.ktu.edu.tr>
2018-03-22 22:34:52 +0300

    masterpage cs

---
+++ MasterPage.master.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Security;
+public partial class MasterPage : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(Session["signup_id"]==null)
+        {
+            Response.Redirect("login.aspx");
+        }  
+    }
+
+
+    protected void ButtonCikis_Click(object sender, EventArgs e)
+    {
+        Session.RemoveAll();
+        Response.Redirect("login.aspx");
+    }
+}
